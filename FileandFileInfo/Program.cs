38a1452267c1b6201f96409197fd45eb59e2a29b
Álvaro Borges <gestor.alvaro@gmail.com>
2023-01-09 22:47:58 -0300

    Add project files.

---
+++ FileandFileInfo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileandFileInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:somefile.jpg";
+            File.Copy(@"C:\temp\myfile.jpg", @"c:\destiny\myfile.jpg", true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                //
+            }
+            var content = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                //
+            }
+            
+
+        }
+    }
+}
